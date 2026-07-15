14ee41fdebd6412195b275f5a6dac483f0fcc32e
saurabh kumar <saurabhkaswa04@gmail.com>
2026-07-15 17:22:56 +0530

    Spring Data JPA with Spring Boot, Hibernate: Hands on 1: Spring Data JPA - Quick Example

---
+++ Week2/ormlearn/src/main/java/com/cognizant/ormlearn/repository/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.cognizant.ormlearn.repository;
+
+import com.cognizant.ormlearn.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, String> {
+
+}
