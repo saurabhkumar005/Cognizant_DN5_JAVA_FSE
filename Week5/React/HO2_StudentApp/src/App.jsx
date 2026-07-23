438a86c969bf89b68d13831dca2491b274b83f17
saurabh kumar <saurabhkaswa04@gmail.com>
2026-07-23 11:05:47 +0530

    Week5: Complete React Handson 2 - StudentApp Components

---
+++ Week5/React/HO2_StudentApp/src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+    return (
+        <div>
+            <Home />
+            <About />
+            <Contact />
+        </div>
+    );
+}
+
+export default App;
